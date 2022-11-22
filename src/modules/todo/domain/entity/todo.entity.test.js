974e22c297194e0f3a9315e8737e33530ab7d3df
Leonardo Pinto <leonardopintodev@gmail.com>
2022-11-22 20:50:20 -0300

    test: todo entity test

--- src/modules/todo/domain/entity/todo.entity.test.js
+++ src/modules/todo/domain/entity/todo.entity.test.js
@@ -1,17 +1,36 @@
-import test from 'node:test';
+import { describe, it } from 'node:test';
 import { strict as assert } from 'node:assert';
 
 import Todo from './todo.entity.js';
 
-test('creates a todo instance with minimum props', (t) => {
-    const todo = new Todo('1', 'user', 'Name');
+describe('Todo Entity Tests', () => {
 
-    assert.ok(todo);
-    assert.equal(todo.id, '1');
-    assert.equal(todo.userId, 'user');
-    assert.equal(todo.name, 'Name');
-    assert.equal(todo.description, undefined);
-    assert.equal(todo.checked, false);
-    assert.ok(todo.createdAt);
-    assert.ok(todo.updatedAt);
+    it('creates a todo instance with minimum props', () => {
+        const todo = new Todo('1', 'user', 'Name');
+
+        assert.ok(todo);
+        assert.equal(todo.id, '1');
+        assert.equal(todo.userId, 'user');
+        assert.equal(todo.name, 'Name');
+        assert.equal(todo.description, undefined);
+        assert.equal(todo.checked, false);
+        assert.ok(todo.createdAt);
+        assert.ok(todo.updatedAt);
+    });
+
+    it('creates a todo instance with all props', () => {
+        const date = new Date(2022, 0, 1);
+        const todo = new Todo('1', 'user', 'Name', 'Description', true, date, date);
+
+        assert.ok(todo);
+        assert.equal(todo.id, '1');
+        assert.equal(todo.userId, 'user');
+        assert.equal(todo.name, 'Name');
+        assert.equal(todo.description, 'Description');
+        assert.equal(todo.checked, true);
+        assert.equal(todo.createdAt, date);
+        assert.equal(todo.updatedAt, date);
+    });
+
 });
+
